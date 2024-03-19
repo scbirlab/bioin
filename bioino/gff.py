0c513859417f515657340439fec34a17aa420f92
Eachan Johnson <eachan.johnson@crick.ac.uk>
2024-03-19 16:40:47 +0000

    Fix filename reporting

--- bioino/gff.py
+++ bioino/gff.py
@@ -698,7 +698,7 @@ class GffFile:
                                 delimiter=sep)
         writer.writeheader()
         
-        print_err('Writing table to {file.name}...')
+        print_err(f'Writing table to {file.name}...')
         for gff_line in self.as_dict():
 
             writer.writerow(gff_line)
